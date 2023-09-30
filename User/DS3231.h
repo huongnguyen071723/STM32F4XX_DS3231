746a5a207ea6190019b780684d3c20bec73f8c79
Huong Nguyen <huongnguyen071723@gmail.com>
2023-09-30 21:11:49 +0700

    Define the I2C address, the data structure of the DS3231 module

--- User/DS3231.h
+++ User/DS3231.h
@@ -18,7 +18,26 @@
 
 /*----- Exposed Macros -----------------------------------------------------------------------------------------------*/
 
+/** 
+ * The slave address byte contains the 7-bit DS3231 address, which is 1101000, 
+ * followed by the direction bit (R/W), which is 0 for a write 
+ */
+#define DS3231_I2C_ADDRESS							0xD0
+
 /*----- Exposed Typedef ----------------------------------------------------------------------------------------------*/
+
+/** Data structure of the DS3231 module */
+typedef struct DS3231_t
+{
+	uint8_t seconds;							/**< second value */
+	uint8_t minutes;							/**< minute value */
+	uint8_t hour;								/**< hours value */
+	uint8_t dayOfWeek;							/**< the day of week */
+	uint8_t dayOfMonth;							/**< the day of month */
+	uint8_t month;								/**< month */
+	uint8_t year;								/**< year */
+} DS3231_t;
+
 
 /*----- Exposed Functions --------------------------------------------------------------------------------------------*/
 
