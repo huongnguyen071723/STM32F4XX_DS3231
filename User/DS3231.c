8494d1c6ad7d6f5c22ccbd71daa303bcfd82a505
Huong Nguyen <huongnguyen071723@gmail.com>
2023-09-30 21:37:30 +0700

    Declare/define functions: convert BCD to DEC and opposite

--- User/DS3231.c
+++ User/DS3231.c
@@ -21,7 +21,57 @@
 
 /*----- Private Functions Declarations -------------------------------------------------------------------------------*/
 
+/**
+ * @brief Convert normal decimal numbers to binary coded decimal 
+ * 
+ * @note Can relocate this function to use another object
+ * @param val 
+ * @return
+ * 
+ */
+static uint8_t decToBcd(int val);
+
+/**
+ * @brief Convert binary coded decimal to normal decimal numbers
+ * 
+ * @note Can relocate this function to use another object
+ * 
+ * @note Can relocate this function to use another object
+ * @param val 
+ * @return
+ */
+static int bcdToDec(uint8_t val);
+
 /*----- Private Functions Definitions --------------------------------------------------------------------------------*/
+
+/**
+ * @internal
+ * @brief Convert normal decimal numbers to binary coded decimal 
+ * 
+ * @note Can relocate this function to use another object
+ * @param val 
+ * @return
+ * 
+ * @endinternal
+ */
+static uint8_t decToBcd(int val)
+{
+	return (uint8_t)((val / 10U * 16U) + (val % 10U));
+}
+
+/**
+ * @internal
+ * @brief Convert binary coded decimal to normal decimal numbers
+ * 
+ * @note Can relocate this function to use another object
+ * @param val 
+ * @return
+ * @endinternal
+ */
+static int bcdToDec(uint8_t val)
+{
+	return (int)((val / 16U * 10U) + (val % 16U));
+}
 
 /*----- Exposed Functions Definitions --------------------------------------------------------------------------------*/
 
