ad13efe1b74ad8ae12a2baa42589839797ae6835
Huong Nguyen <huongnguyen071723@gmail.com>
2023-10-01 16:19:28 +0700

    Declare/define the DS3231 APIs

--- User/DS3231.c
+++ User/DS3231.c
@@ -12,6 +12,9 @@
 /*----- Inclusions ---------------------------------------------------------------------------------------------------*/
 
 #include "DS3231.h"
+#include "common.h"
+
+extern I2C_HandleTypeDef hi2c1;
 
 /*----- Private Macros -----------------------------------------------------------------------------------------------*/
 
@@ -74,5 +77,120 @@ static int bcdToDec(uint8_t val)
 }
 
 /*----- Exposed Functions Definitions --------------------------------------------------------------------------------*/
+
+/**
+ * @internal
+ * @brief Transmit data to the DS3231 module via I2C protocol
+ * 
+ * 
+ * @param pBuffer 
+ * @param dataLen 
+ * @endinternal
+ */
+void i2c_transmit_data(uint8_t *pBuffer, uint16_t dataLen)
+{
+	if ((pBuffer == NULL) || (dataLen == 0U))
+	{
+		return;
+	}
+	else
+	{
+		HAL_I2C_Mem_Write(&hi2c1, DS3231_I2C_ADDRESS, 0x00, 1, pBuffer, dataLen, 1000);
+	}
+}
+
+/**
+ * @internal
+ * @brief Receive data from the DS3231 module via I2C protocol
+ * 
+ * @param pBuffer 
+ * @param dataLen 
+ * @endinternal
+ */
+void i2c_receive_data(uint8_t *pBuffer, uint16_t dataLen)
+{
+	if ((pBuffer == NULL) || (dataLen == 0U))
+	{
+		return;
+	}
+	else
+	{
+		HAL_I2C_Mem_Read(&hi2c1, DS3231_I2C_ADDRESS, 0x00, 1, pBuffer, dataLen, 1000);
+	}
+}
+
+/**
+ * @internal
+ * @brief Get time data from the DS3231 module
+ * 
+ * 
+ * @param pTime 
+ * @endinternal
+ */
+void ds3231_get_time(DS3231_t *pTime)
+{
+	if (pTime == NULL)
+	{
+		return;
+	}
+	else
+	{
+		uint8_t tmpBuffer[7U];
+
+		i2c_receive_data(tmpBuffer, sizeof(tmpBuffer));
+
+		pTime->seconds = bcdToDec(tmpBuffer[0U]);
+		pTime->minutes = bcdToDec(tmpBuffer[1U]);
+		pTime->hour = bcdToDec(tmpBuffer[2U]);
+		pTime->dayOfWeek = bcdToDec(tmpBuffer[3U]);
+		pTime->dayOfMonth = bcdToDec(tmpBuffer[4U]);
+		pTime->month = bcdToDec(tmpBuffer[5U]);
+		pTime->year = bcdToDec(tmpBuffer[6U]);
+	}
+}
+
+/**
+ * @internal
+ * @brief Set time data to the DS3231 module
+ * 
+ * 
+ * @param pTime 
+ * @endinternal
+ */
+void ds3231_set_time(DS3231_t *pTime)
+{
+	if (pTime == NULL)
+	{
+		return;
+	}
+	else
+	{
+		uint8_t tmpBuffer[7U];
+		tmpBuffer[0U] = decToBcd(pTime->seconds);
+		tmpBuffer[1U] = decToBcd(pTime->minutes);
+		tmpBuffer[2U] = decToBcd(pTime->hour);
+		tmpBuffer[3U] = decToBcd(pTime->dayOfWeek);
+		tmpBuffer[4U] = decToBcd(pTime->dayOfMonth);
+		tmpBuffer[5U] = decToBcd(pTime->month);
+		tmpBuffer[6U] = decToBcd(pTime->year);
+
+		i2c_transmit_data(tmpBuffer, sizeof(tmpBuffer));
+	}
+}
+
+/**
+ * @internal
+ * @brief Get temperature from the DS3231 module
+ * 
+ * 
+ * @param  
+ * @endinternal
+ */
+float ds3231_get_temperature(void)
+{
+	float retVal = 0;
+
+	return retVal;
+}
 
 /** EOF */
--- User/DS3231.h
+++ User/DS3231.h
@@ -41,6 +41,47 @@ typedef struct DS3231_t
 
 /*----- Exposed Functions --------------------------------------------------------------------------------------------*/
 
+/**
+ * @brief Transmit data to the DS3231 module via I2C protocol
+ * 
+ * 
+ * @param pBuffer 
+ * @param dataLen 
+ */
+void i2c_transmit_data(uint8_t *pBuffer, uint16_t dataLen);
+
+/**
+ * @brief Receive data from the DS3231 module via I2C protocol
+ * 
+ * @param pBuffer 
+ * @param dataLen 
+ */
+void i2c_receive_data(uint8_t *pBuffer, uint16_t dataLen);
+
+/**
+ * @brief Get time data from the DS3231 module
+ * 
+ * 
+ * @param pTime 
+ */
+void ds3231_get_time(DS3231_t *pTime);
+
+/**
+ * @brief Set time data to the DS3231 module
+ * 
+ * 
+ * @param pTime 
+ */
+void ds3231_set_time(DS3231_t *pTime);
+
+/**
+ * @brief Get temperature from the DS3231 module
+ * 
+ * 
+ * @param  
+ */
+float ds3231_get_temperature(void);
+
 #endif /* DS3231_H */
 
 /** EOF */
