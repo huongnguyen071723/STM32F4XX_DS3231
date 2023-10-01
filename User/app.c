b9e7835d8cc3c520d2dc618e6610b2b4f4090615
Huong Nguyen <huongnguyen071723@gmail.com>
2023-10-01 17:11:27 +0700

    Add testing code

--- User/app.c
+++ User/app.c
@@ -13,12 +13,23 @@
 
 #include "app.h"
 #include "common.h"
+#include "DS3231.h"
 
 /*----- Private Macros -----------------------------------------------------------------------------------------------*/
 
 /*----- Private Typedef ----------------------------------------------------------------------------------------------*/
 
 /*----- Private Variables --------------------------------------------------------------------------------------------*/
+
+static DS3231_t Time = 
+{
+	.seconds = 15U,
+	.minutes = 15U,
+	.hour = 15U,
+	.dayOfMonth = 15U,
+	.month = 5U,
+	.year = 23U
+};
 
 /*----- Private Functions Declarations -------------------------------------------------------------------------------*/
 
@@ -37,9 +48,15 @@ int app_main()
 {
 	PRINTF("app_main\r\n");
 
+	ds3231_set_time(&Time);
 	while (1)
 	{
 
+		ds3231_get_time(&Time);
+
+		PRINTF("%02d %02d %02d %02d %02d %02d\r\n", Time.seconds, Time.minutes, Time.hour, Time.dayOfMonth, Time.month, Time.year);
+
+		HAL_Delay(500U);
 	}
 
 	return 0;
